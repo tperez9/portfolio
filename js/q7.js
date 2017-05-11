1b22b7e577bc3185bb8cc19971394d80320e81f6
tperez9 <tperez9@unm.edu>
2017-05-10 20:40:24 -0600

    Create q7.js

---
+++ js/q7.js
@@ -0,0 +1,54 @@
+var projection = ol.proj.get('EPSG:4326');
+
+var A5_cont_st = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+		attributions: new ol.Attribution({
+			html: 'GeoServer'
+		}),
+		params: {'LAYERS':'ws_tperez:35106-A5_CONT_ST'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/ws_tperez/wms?version=1.1.1&request=GetCapabilities',
+		serverType: 'geoserver',
+		projection: projection,
+		format: new ol.format.KML({
+			extractStyles:false
+		})
+	})
+})
+
+var roads_st = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+		attributions: new ol.Attribution({
+			html: 'GeoServer'
+		}),
+		params: {'LAYERS':'ws_tperez:tgr2006se_bern_lka_ST'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/ws_tperez/wms?version=1.1.1&request=GetCapabilities',
+		serverType: 'geoserver',
+		projection: projection,
+		format: new ol.format.KML({
+			extractStyles:false
+		})
+	})
+})
+var Layer_Stamen_terrain = new ol.layer.Group({
+      layers: [
+          new ol.layer.Tile({
+              source: new ol.source.Stamen({layer: 'terrain'})
+          })
+      ]
+  });
+  var map = new ol.Map({
+  	target: 'map_q6',
+  	layers: [
+  		Layer_Stamen_terrain,
+  		A5_cont_st,
+ 	  	roads_st
+  	],
+  	view: new ol.View({
+ 		center: ol.proj.fromLonLat([-106.610, 35.06]),
+ 		zoom: 11
+  	}),
+  	controls: ol.control.defaults().extend([
+  		new ol.control.ScaleLine()
+  	]),
+  	
+  });
